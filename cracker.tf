2a94c8741c81ba7f4d5eab44a25424b2d3204502
Luciano Mammino <lucianomammino@gmail.com>
2018-06-07 23:27:56 +0100

    Adding awslogs-agent

--- cracker.tf
+++ cracker.tf
@@ -6,3 +6,23 @@ variable "num_clients" {
   type    = "string"
   default = 4
 }
+
+# Create policy with
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [
+#         "logs:CreateLogGroup",
+#         "logs:CreateLogStream",
+#         "logs:PutLogEvents",
+#         "logs:DescribeLogStreams"
+#     ],
+#       "Resource": [
+#         "arn:aws:logs:*:*:*"
+#     ]
+#   }
+#  ]
+# }
+
